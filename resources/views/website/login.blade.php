a092fe419aa96b7e662d574a9541e9912555d90a
Guilherme  Devólio <61203395+Guilherme5889@users.noreply.github.com>
2021-02-07 22:54:05 -0300

    Change HTML Sintax

--- resources/views/website/login.blade.php
+++ resources/views/website/login.blade.php
@@ -8,17 +8,19 @@
        <div class="col-12 p-4  ">
          
         <form>
+        
             <div class="mb-3">
               <label for="exampleInputEmail1" class="form-label">Username</label>
               <input type="email" placeholder="Type your username"  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-
             </div>
+            
             <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">Password</label>
               <input type="password" placeholder="Type your password" class="form-control" id="exampleInputPassword1">
             </div>
 
             <button type="submit" class="btn bg-purple text-light">Login</button>
+            
           </form>
          
        </div>
